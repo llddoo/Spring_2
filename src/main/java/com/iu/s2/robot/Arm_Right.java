87c8c3891ab970371092833c066f907a420aa389
llddoo <74532161+llddoo@users.noreply.github.com>
2021-03-22 12:22:47 +0900

    늦게 브렌치

--- src/main/java/com/iu/s2/robot/Arm_Right.java
+++ src/main/java/com/iu/s2/robot/Arm_Right.java
@@ -1,8 +1,10 @@
 package com.iu.s2.robot;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("ar")
 public class Arm_Right extends Arm {
 	
 	public Arm_Right() {
