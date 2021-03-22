04fbf1bde605e6f0f7569900f6cb9f8a3d55569d
llddoo <74532161+llddoo@users.noreply.github.com>
2021-03-22 12:22:19 +0900

    DiTEST_1에 해야하는데 늦게 브렌치

---
+++ src/main/java/com/iu/s2/robot/Arm_Left.java
@@ -0,0 +1,11 @@
+package com.iu.s2.robot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Arm_Left extends Arm {
+
+	public Arm_Left() {
+		this.setName("Left Arm");
+	}
+}
---
+++ src/main/java/com/iu/s2/robot/Arm_Right.java
@@ -0,0 +1,11 @@
+package com.iu.s2.robot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Arm_Right extends Arm {
+	
+	public Arm_Right() {
+		this.setName("Right Arm");
+	}
+}
